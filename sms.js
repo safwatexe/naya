fd33c6ce1dc3cb960aeb7b9c9affd7b3aab07f6c
naya97 <78640234+naya97@users.noreply.github.com>
2021-02-06 13:00:24 +0300

    Add files via upload

---
+++ sms.js
@@ -0,0 +1,12 @@
+
+function addtolist(){
+let tel=document.getElementById("telN");  //textbox den value alma
+let txt=document.getElementById("result").textContent;  //text te value kaydetme
+document.getElementById("result").textContent=txt; //yazdırma
+}
+
+
+
+
+
+
